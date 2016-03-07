49271cbd13991a790f469f3c5288fbd305af0c7c
cowiem54 <cowiem54@uws20.acs.uwosh.edu>
2016-03-07 14:47:23 -0600

    Here's my changes temp

--- Exercises/Development/Booth_ex3.js
+++ Exercises/Development/Booth_ex3.js
@@ -10,49 +10,27 @@
 
 	// Initialise the exercise
 	initArr: function(arr_size) {
+	    var i;
+	    var next_val;
 	    var array_str;
 	    my_array = ["00","01","10","11"];
-		next_val = Math.floor(Math.random() * 4);
+		answer = Math.floor(Math.random() * 4);
+		console.log(" " + next_val);
 	    array_str = "";
-		array_str = my_array[answer];
+
+		array_str = array_str + " " + my_array[answer];
+
 	    console.log(array_str);
 	    return array_str;
 	},
 	
 	maxValue: function() {
-	    var i;
-	    var temp;
-	    var temp_index;
-	    // Get the largest in index 0
-	    for (i = 1; i < my_array.length; i++) {
-		if (my_array[i] > my_array[0]) {
-		    temp = my_array[0];
-		    my_array[0] = my_array[i];
-		    my_array[i] = temp;
-		}
-	    }
-
-	    // The largest is now in index 0.
-	    // Get our three options in index 1, 2, 3
-	    temp_index = Math.floor(Math.random() * (my_array.length-1)) + 1;
-	    temp = my_array[1];
-	    my_array[1] = my_array[temp_index];
-	    my_array[temp_index] = temp;
-
-	    temp_index = Math.floor(Math.random() * (my_array.length-1)) + 2;
-	    temp = my_array[2];
-	    my_array[2] = my_array[temp_index];
-	    my_array[temp_index] = temp;
-
-	    temp_index = Math.floor(Math.random() * (my_array.length-1)) + 3;
-	    temp = my_array[3];
-	    my_array[3] = my_array[temp_index];
-	    my_array[temp_index] = temp;
 
 	    booth_ex3.option[0] = my_array[1];
 	    booth_ex3.option[1] = my_array[2];
 	    booth_ex3.option[2] = my_array[3];
-	    return my_array[0];
+		
+	    return answer;
 	},
 
     };
