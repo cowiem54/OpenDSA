40aaff26334b9b511b54eae20d2d303e795c90c8
cowiem54 <cowiem54@utl18.acs.uwosh.edu>
2016-03-09 15:57:23 -0600

    sample question working

--- Exercises/Development/Booth_ex3.js
+++ Exercises/Development/Booth_ex3.js
@@ -15,21 +15,15 @@
 
 		answerIndex = Math.floor(Math.random() * 4);
 
-		var displayBits = "Test: " + bitCodes[answerIndex];
+		var displayBits = "Bits: " + bitCodes[answerIndex];
 
 	    return displayBits;
 	},
 	
-<<<<<<< HEAD
 	maxValue: function() {
-		if(answer==0||answer==3){
-			booth_ex3.option[0] = "Subtract Multiplicand";
-=======
-	getAnswer: function() {
 
 		if(answerIndex == 0 || answerIndex == 3) {
 			booth_ex3.option[0] = "Do Nothing";
->>>>>>> e06de7c44df6a10a4d60dfe75b0b7239209b8cb0
 			booth_ex3.option[1] = "Add Multiplicand";
 			return "Do Nothing";
 		}
@@ -44,12 +38,7 @@
 			return "Subtract Multiplicand";
 		}
 
-<<<<<<< HEAD
-	    
-=======
 
-	    return answerIndex;
->>>>>>> e06de7c44df6a10a4d60dfe75b0b7239209b8cb0
 	},
 
     };
